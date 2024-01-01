3617bb5a1bde0fb2ca9866a6e11c9944b20b3bf0
Divyansh <divyanshsrivastav72@gmail.com>
2024-01-01 13:49:54 +0530

    UpdateLinks

--- src/constants/index.js
+++ src/constants/index.js
@@ -90,18 +90,18 @@ export const footerLinks = [
     {
         title: "Products",
         links: [
-            { name: "Air Force 1", link: "/" },
-            { name: "Air Max 1", link: "/" },
-            { name: "Air Jordan 1", link: "/" },
-            { name: "Air Force 2", link: "/" },
-            { name: "Nike Waffle Racer", link: "/" },
-            { name: "Nike Cortez", link: "/" },
+            { name: "Air Force 1", link: "https://www.nike.com/in/w/air-force-1-shoes-5sj3yzy7ok" },
+            { name: "Air Max 1", link: "https://www.nike.com/in/w/air-max-shoes-a6d8hzy7ok" },
+            { name: "Air Jordan 1", link: "https://www.nike.com/in/w/jordan-1-shoes-4fokyzy7ok" },
+            { name: "Air Force 2", link: "https://www.kickscrew.com/collections/air-force-2" },
+            { name: "Nike Waffle Racer", link: "https://www.nike.com/in/w/nike-waffle-shoes-2wk64zy7ok" },
+            { name: "Nike Cortez", link: "https://www.nike.com/in/w/cortez-shoes-byfxzy7ok" },
         ],
     },
     {
         title: "Help",
         links: [
-            { name: "About us", link: "/" },
+            { name: "About us", link: "https://github.com/divyaannsh" },
             { name: "FAQs", link: "/" },
             { name: "How it works", link: "/" },
             { name: "Privacy policy", link: "/" },
@@ -111,8 +111,8 @@ export const footerLinks = [
     {
         title: "Get in touch",
         links: [
-            { name: "customer@nike.com", link: "mailto:customer@nike.com" },
-            { name: "+92554862354", link: "tel:+92554862354" },
+            { name: "divyansh@nike.com", link: "mailto:divyanshsrivastav72@gmail.com" },
+            { name: "+91 9560350477", link: "tel:+91 9560350477" },
         ],
     },
 ];
